4dd3505e81ce427d8306ff8e761ced9deb8e5c48
Luc Sorel <luc.sorel@gmail.com>
2013-03-25 00:22:11 +0100

    [MAJ] definition of the FileUpload directive to use a hidden iframe to post the mp3 file

--- tag-bpm/www/js/bpmApp.js
+++ tag-bpm/www/js/bpmApp.js
@@ -1,91 +1,7 @@
 'use_strict';
-/*
- *<a file-post="{inputId:'testFileInputId1', action:'api/swingFileOut/index.php', callback:'testUpload(success, content)'}">Swing file out!</a>
- */
-angular.module('filePost', []).directive('filePost', function($parse) {
-	return {
-		// only "filePost" attribute use for the moment
-		transclude: true,
-		scope: {
-			id: '=',
-			action: '=',
-			callback: '&'
-		},
-		restrict: 'E',
-		replace: true,
-		template: "<div class='filepost'><form id='upload_form_{id}' method='post' action='{action}'>"
-			+"</form>"
-			+"</div>",
-		compile: function() {},
-		link: function(scope, element, attrs) {
-			// initializes an array of link errors
-			var linkErrors = [];
 
-			// retrieves the content of the filePost attribute
-			var filePostContent = attrs.filePost;
-			var filePostConfig = scope.$eval(filePostContent);
-			var fileInputId = null;
-			var inputFileElement = null;
-			var formAction = null;
-			var callbackFunction = null;
-			if (! angular.isObject(filePostConfig)) {
-				linkErrors.push('wrong file-post syntax');
-			}
-			else {
-				// checks the reference to the file input id
-				fileInputId = filePostConfig.inputId;
-				if (! fileInputId) {
-					linkErrors.push('missing inputId in ' + filePostContent);
-				}
-				else {
-					inputFileElement = scope.$document.getElementById(fileInputId);
-					if (angular.isNull(inputFileElement)) {
-						linkErrors.push('could not retrieve the input file #' + fileInputId);
-					}
-				}
-				// retrieves the post action or use the current location
-				formAction = filePostConfig.action;
-				if (! formAction) {
-					formAction = scope.$window.location.href;
-				}
-				// ensures that the post callback function exists in the scope
-				if (! filePostConfig.callback) {
-					linkErrors.push('missing callback in ' + filePostContent);
-				}
-				else {
-					callbackFunction = $parse(filePostConfig.callback);
-					if (! angular.isFunction(callbackFunction)) {
-						linkErrors.push(filePostConfig.callback + ' does not resolve to scope function in' + filePostContent);
-					}
-				}
-			}
-			// throws an exception if errors were found
-			if (linkErrors.length > 0) {
-				throw angular.toJson(linkErrors);
-			}
-
-			// from here, the directive config is fine :)
-			
-			//creates a form
-			// binds a click handler on the element to create the hidden iframe
-			element.bind('click' function($event) {
-				// prevents default behavior of the click event
-				$event.preventDefault = true;
-
-				// creates a hidden iframe with a post form
-				var postIframeId = fileInputId + '_frame';
-				var postIframe = angular.element("<iframe id='" + postIframeId + "' name='" + postIframeId 
-						+ "' border='0' width='0' height='0' style='width: 0px; height: 0px; border: none; display: none' />");
-
-				// adds the hidden iframe before the file input
-				document.body.insertBefore(postIframe, element);
-			});
-		}
-	};
-});
-
-// the BPM application depends on Angular-UI module
-var bpmApp = angular.module('BpmApp', ['ui', 'filePost']);
+// the BPM application depends on the angularUI and fileUpload modules
+var bpmApp = angular.module('BpmApp', ['ui', 'fileUpload']);
 
 BpmCtrl = function($scope, $http) {
 	// the filename
@@ -97,20 +13,19 @@ BpmCtrl = function($scope, $http) {
 		return linkClass;
 	};
 
-	$scope.swingFileOut = function() {
-		// prevents 2ble posting the file
-		if (!$scope.swingingFileOut) {
-			$scope.swingingFileOut = true;
-			 $http({
-		            method: 'POST',
-		            url: 'api/swingFileOut/index.php',
-		            data: {swingOutFile: document.getElementById('swingOutFile').files[0]},
-		            headers: {'Content-Type': 'multipart/form-data'}
-		       }).success(function(data){
-		            alert(data);
-		       });
+	$scope.testUpload = function(success, content) {
+		// manages the returned content of the query
+		if (success) {
+			$scope.swingingFileOut = false;
+			alert(content);
+			resetFilePicker();
+			$scope.swingOutFileName = null;
 		}
-	};
+		// prevents 2ble posting the file
+		else {
+			$scope.swingingFileOut = true;
+		}
+	}
 
 	$scope.onSwingOutFileChange = function(fileInput) {
 		if (fileInput && angular.isString(fileInput.value) && (fileInput.value.length > 0)) {
@@ -122,8 +37,8 @@ BpmCtrl = function($scope, $http) {
 	};
 };
 
-$(document).ready(bpmJqueryBind);
-function bpmJqueryBind() {
+$(document).ready(resetFilePicker);
+function resetFilePicker() {
 	// clears the file picker field
 	$('input[name="swingOutFile"]').val('');
 };
---
+++ tag-bpm/www/js/file-upload.js
@@ -0,0 +1,112 @@
+'use_strict';
+
+/**
+ * Inspired from http://twilson63.github.com/ngUpload/:
+ * - links the iframe to a form id instead of nesting them
+ * - fixed a bug when inserted the iframe with jQuery
+ */
+angular.module('fileUpload', []).directive('fileUploadForm', ['$parse', function($parse) {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			var formId = attrs.id;
+			if (! angular.isString(formId) && formId.length < 1) {
+				throw 'using the file-upload-form directive on a form which has no HTML id';
+			}
+
+			element.attr('target', formId + '_frame');
+	        element.attr('method', 'post');
+	        // appends a timestamp field to the url to prevent browser caching results
+	        element.attr('action', element.attr('action') + '?_t=' + new Date().getTime());
+	        element.attr('enctype', 'multipart/form-data');
+	        element.attr('encoding', 'multipart/form-data');
+		}
+	};
+}]).directive('fileUploadConfig', ['$parse', function($parse) {
+	return {
+		// only "fileUploadConfig" attribute use for the moment
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			// initializes an array of link errors
+			var linkErrors = [];
+
+			// retrieves the config from the fileUploadConfig attribute
+			var fileUploadConfigContent = attrs.fileUploadConfig;
+			var fileUploadConfig = scope.$eval(fileUploadConfigContent);
+			var fileUploadFormId = null;
+			var uploadFormElement = null;
+			var callbackFunction = null;
+
+			if (! angular.isObject(fileUploadConfig)) {
+				linkErrors.push('wrong file-upload-config syntax');
+			}
+			else {
+				// checks the reference to the file input id
+				fileUploadFormId = fileUploadConfig.formId;
+				if (! fileUploadFormId) {
+					linkErrors.push('missing inputId in ' + fileUploadConfigContent);
+				}
+				else {
+					uploadFormElement = document.getElementById(fileUploadFormId);
+					if (! angular.isElement(uploadFormElement)) {
+						linkErrors.push('could not retrieve the form #' + fileUploadFormId);
+					}
+				}
+				// ensures that the post callback function exists in the scope
+				if (! fileUploadConfig.callback) {
+					linkErrors.push('missing callback in ' + fileUploadConfigContent);
+				}
+				else {
+					callbackFunction = $parse(fileUploadConfig.callback);
+					if (! angular.isFunction(callbackFunction)) {
+						linkErrors.push(fileUploadConfig.callback + ' does not resolve to scope function in' + fileUploadConfigContent);
+					}
+				}
+			}
+			// throws an exception if errors were found
+			if (linkErrors.length > 0) {
+				throw angular.toJson(linkErrors);
+			}
+
+			// from here, the directive config is fine :)
+			
+			// binds a click handler on the element to create the hidden iframe
+			element.bind('click', function($event) {
+				// prevents default behavior of the click event
+				$event.preventDefault = true;
+
+				// creates a hidden iframe with a post form
+				var postIframeId = fileUploadFormId + '_frame';
+				var postIframe = angular.element("<iframe id='" + postIframeId + "' name='" + postIframeId 
+						+ "' border='0' width='0' height='0' style='width: 0px; height: 0px; border: none; display: none' />");
+
+				// attach function to load event of the iframe
+				postIframe.bind('load', function () {
+					// get content - requires jQuery
+					var content = postIframe.contents().find('body').text();
+
+		            // executes the upload callback in the active scope
+					scope.$apply(function () { 
+						callbackFunction(scope, {success: true, content: content});
+					});
+
+					// removes iframe with a timeout to fix a bug in Google Chrome which disposes the iframe before content is ready
+		            if (content !== "") {
+		                setTimeout(function() {postIframe.remove();}, 250);
+		            }
+				});
+
+				// adds the hidden iframe (use '[0]' because of jQuery) before the file input
+				uploadFormElement.insertBefore(postIframe[0], null);
+
+				// calls the callback on the scope to let it know that the form is posting
+				scope.$apply(function() { 
+					callbackFunction(scope, {success: false, content: 'posting'});
+				});
+
+				// submits the form
+				uploadFormElement.submit();
+			})
+		}
+	};
+}]);
